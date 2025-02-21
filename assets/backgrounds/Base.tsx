0f7dcfbaec0009fc6150f6be511c5200ff0afdfe
Esc <akeldiantelas@gmail.com>
2025-02-21 19:04:11 -0300

    First commit

---
+++ assets/backgrounds/Base.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="Base" tilewidth="8" tileheight="8" tilecount="1600" columns="40">
+ <image source="tileset.aseprite" width="320" height="320"/>
+</tileset>
